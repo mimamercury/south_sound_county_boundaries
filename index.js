4ac627d3a56c94e8e505d3c8a591110d5eeb670f
sethvincent <sethvincent@gmail.com>
2023-12-06 09:35:28 -0800

    💾

--- index.js
+++ index.js
@@ -5,12 +5,19 @@ import * as dirname from '@editorialapp/datatools/dirname'
 const dataDirectory = dirname.join(import.meta.url, 'data')
 const thurstonFilepath = path.join(dataDirectory, 'thurston.geojson')
 
-const thurstonGeojson = await readJson(thurstonFilepath)
+const boundaries = {}
+boundaries.thurston = await readJson(thurstonFilepath)
 
 /**
  * @param {'thurston'} county_slug 
  * @return {Object} GeoJSON of county boundary
  */
 export function getGeojson (county_slug) {
-    return thurstonGeojson
+    const geojson = boundaries[county_slug]
+
+    if (!geojson) {
+        throw new Error(`GeoJSON not found for county_slug ${county_slug}`)
+    }
+
+    return geojson
 }
